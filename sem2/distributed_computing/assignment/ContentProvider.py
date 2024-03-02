fa41d0aa6b8d7d0088071eabd40380866aee67a2
Basavaraj Ainapur <basavarajainapur@Basavarajs-MacBook-Air.local>
2024-03-02 16:38:58 +0530

    DC assignment

---
+++ sem2/distributed_computing/assignment/ContentProvider.py
@@ -0,0 +1,8 @@
+
+from jsonrpcclient import request
+import requests
+
+url = 'http://localhost:8080/rpc'
+
+result = requests.post(url, json=request('addfile', params={"filename" : "abc.txt", "data": "123456"}))
+print(result)
